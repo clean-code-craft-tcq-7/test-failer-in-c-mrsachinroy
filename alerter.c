6639a270e5b4569ff1cf1d57d3cc3e0387a167ee
SACHIN ROY <sachinkumarsnp@gmail.com>
2022-11-30 12:03:58 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <assert.h>
 
+
+#define TEST_ENV    (1)
+#define PROD_ENV    (2)
+
+#define ENV     TEST_ENV
+
 int alertFailureCount = 0;
 
+//for testing purpose
 int networkAlertStub(float celcius) {
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
+    return 200;
+}
+
+//fucntion for actual network alert
+int networkAlert(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok
     // Return 500 for not-ok
@@ -13,7 +29,11 @@ int networkAlertStub(float celcius) {
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
+#if ENV == TEST_ENV
     int returnCode = networkAlertStub(celcius);
+#elif ENV == PROD_ENV
+    int returnCode = networkAlert(celcius);
+#endif
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
@@ -21,6 +41,9 @@ void alertInCelcius(float farenheit) {
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
+#if ENV == TEST_ENV
+    assert((returnCode != 200) && (alertFailureCount <1));
+#endif
 }
 
 int main() {
