352a27982d7e8d55045fe1b9028162034f2bc89a
SACHIN ROY <sachinkumarsnp@gmail.com>
2022-11-30 09:32:17 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define MAX_SIZE    50
+#define MIN_SIZE    25
+
 char size(int cms) {
     char sizeName = '\0';
     if(cms < 38) {
@@ -17,6 +20,15 @@ int main() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
+
+    for (size_t i = MIN_SIZE; i < MAX_SIZE; i++)
+    {
+        if(size(i) == '\0')
+        {
+            assert(0);
+        }
+    }
+    
     printf("All is well (maybe!)\n");
     return 0;
 }
