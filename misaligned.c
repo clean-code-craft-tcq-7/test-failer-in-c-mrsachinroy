b79ece019637b5e497d8fe7c9635082a31ee6d7d
mrsachinroy <sachinkumarsnp.com>
2022-12-10 12:41:52 +0530

    tEMP

--- misaligned.c
+++ misaligned.c
@@ -1,13 +1,42 @@
 #include <stdio.h>
 #include <assert.h>
+#include "string.h"
+#include "stdlib.h"
+
+
+//converting into string
+const char* ConvertToString(int ColourCode, const char* MajorClr[], const char* MinorClr[] )
+{
+    char retString[20] = {};
+    sprintf(retString,"%d",ColourCode);
+    strcat(retString," | ");
+    strcat(retString,MajorClr[0]);
+    strcat(retString," | ");
+    strcat(retString,MinorClr[0]);
+    const char * ptrretString = retString;
+    return ptrretString;
+}
+
 
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    const char *ptrPrintString = NULL;
+    int indx = 0;
+    char TstString[7] = {};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+
+            ptrPrintString = ConvertToString(i * 5 + j, majorColor+i, minorColor+i);
+            printf("%s \n",ptrPrintString);
+
+            if (i > 0)
+            {
+                //testing for repeating of minor colour value for consecutive colour code 
+                assert(strcmp(TstString,ptrPrintString + (strlen(ptrPrintString) - 6)));
+            }
+            strcpy(TstString,ptrPrintString + (strlen(ptrPrintString) - 6));
         }
     }
     return i * j;
@@ -15,19 +44,7 @@ int printColorMap() {
 
 int main() {
     
-    printf("All is well (maybe!!!!!!!!!!======!!!!!!)\n");
-    FILE *ClrMapOut = freopen("output.txt", "w", stdout);
     int result = printColorMap();
-    fclose(ClrMapOut);
-    // fflush( stdout );
-    
-    printf("All is well =============================================(maybe!)\n");
-    fclose(stdout);
-    
-    printf("All is well =============================================(maybe!)\n");
-    setvbuf (stdout, NULL, _IONBF, 0);
-    assert(result == 5);
-    printf("All is well =============================================(maybe!)\n");
 
     return 0;
 }
