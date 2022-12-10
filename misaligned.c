fde83ad9ae8d1f7a6d30ed5ef070816657c30bf2
SACHIN ROY <70900516+mrsachinroy@users.noreply.github.com>
2022-12-10 14:45:48 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -4,20 +4,40 @@
 #include "stdlib.h"
 
 
+char retString[20] = {};
+
+//checking the alignment
+void TestAlignment(const char* TstString, char Seprator)
+{
+    static int SepPos, i;
+    char * ptr = NULL;
+    ptr = strchr( TstString, Seprator );
+
+    if (i < 1)
+    {
+        SepPos = ptr - TstString;
+        i++;
+    }
+    else
+    {
+        assert(SepPos == ptr - TstString);
+        SepPos = ptr - TstString;
+    }
+}
+
 //converting into string
 const char* ConvertToString(int ColourCode, const char* MajorClr[], const char* MinorClr[] )
 {
-    char retString[20] = {};
     sprintf(retString,"%d",ColourCode);
     strcat(retString," | ");
     strcat(retString,MajorClr[0]);
     strcat(retString," | ");
     strcat(retString,MinorClr[0]);
-    const char * ptrretString = retString;
+    const char * ptrretString = (const char*)retString;
     return ptrretString;
 }
 
-
+// printing the colour map
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
@@ -29,8 +49,10 @@ int printColorMap() {
         for(j = 0; j < 5; j++) {
 
             ptrPrintString = ConvertToString(i * 5 + j, majorColor+i, minorColor+i);
-            printf("%s \n",ptrPrintString);
+            printf("%s \n", ptrPrintString);
 
+            TestAlignment(ptrPrintString, '|');
+            
             if (i > 0)
             {
                 //testing for repeating of minor colour value for consecutive colour code 
@@ -41,6 +63,8 @@ int printColorMap() {
     }
     return i * j;
 }
+
+
 
 int main() {
     
